20c1e939de7c5284edfd0f67d4481e527eea13af
peada04 <dpeacock@hashicorp.com>
2024-07-15 16:56:16 -0400

    adding updates and files directory

--- main.tf
+++ main.tf
@@ -18,6 +18,7 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
+#test
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
